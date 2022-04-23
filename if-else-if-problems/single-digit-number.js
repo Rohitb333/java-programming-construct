60acb0852169f3bac3a5bb92243ffbb2bd72707e
Rohit Bansod <97388524+Rohitb333@users.noreply.github.com>
2022-04-23 23:21:16 +0530

    Added Single Digit Number File

---
+++ if-else-if-problems/single-digit-number.js
@@ -0,0 +1,24 @@
+/* Question 1:
+ Read a single digit number and write the number in word
+*/
+let number = Math.round(Math.random() * 10 + 1);
+
+if (number == 1) {
+    console.log("Number = ONE");
+} else if (number == 2) {
+    console.log("Number = TWO");
+} else if (number == 3) {
+    console.log("Number = THREE");
+} else if (number == 4) {
+    console.log("Number = FOUR");
+} else if (number == 5) {
+    console.log("Number = FIVE");
+} else if (number == 6) {
+    console.log("Number = SIX");
+} else if (number == 7) {
+    console.log("Number = SEVEN");
+} else if (number == 8) {
+    console.log("Number = EIGHT");
+} else {
+    console.log("Number = NINE");
+}
